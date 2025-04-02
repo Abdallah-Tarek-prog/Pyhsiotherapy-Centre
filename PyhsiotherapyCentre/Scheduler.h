a85068f0131d8171214d247a56c9e586ab824e27
Abdulrahman Jalal <67074750+abdulrhman-dev@users.noreply.github.com>
2025-04-02 19:24:55 +0200

    Organized the Scheduler code.

--- PyhsiotherapyCentre/Scheduler.h
+++ PyhsiotherapyCentre/Scheduler.h
@@ -11,16 +11,17 @@ class Scheduler
         int timeStep;
         LinkedQueue<Patient*> allPatientsList;
         MpriQueue1<Patient*> earlyList;
-        M1Queue<Patient*> U_WaitingList;
-        M1Queue<Patient*> E_WaitingList;
-        M2Queue<Patient*> X_WaitingList;
+        //M1Queue<Patient*> U_WaitingList;
+        //M1Queue<Patient*> E_WaitingList;
+        //M2Queue<Patient*> X_WaitingList;
         priQueue<Patient*> lateList;
         
     public:
         Scheduler()
         {
-            srand(time(0))
+            srand(time(0));
         }
+
         ~Scheduler()
         {
         }
@@ -28,9 +29,10 @@ class Scheduler
         void MoveFromAll(){
             Patient* topPatient;
             
-            while(allPatientsList.peek(topPatient))){
+            while(allPatientsList.peek(topPatient)){
                 if(topPatient->getVT() == timeStep){
-                    allPatientsList.dequeue();
+                    Patient* temp;
+                    allPatientsList.dequeue(temp);
                     
                     if(topPatient->getVT() == topPatient->getPT()){
                         RandomWaiting(topPatient);
@@ -49,92 +51,87 @@ class Scheduler
         }
         
 
-        
+      void simulateTimestep()
+       {    
+        MoveFromAll();
 
+        Patient * next;
 
+        int X = rand() % 101;
 
-        
-
-      / void simulateTimestep()
-      / {            MoveFromAll();
-            int X = rand() % 101;
-            Patient * next;
-            switch (X / 10) {
-                case 0:
-                    earlyList.dequeue(next);
-                    RandomWaiting(next);
-                    break;
-                case 1:
-                    lateList.dequeue(next);
-                    RandomWaiting(next);
-                    break;
-                case 2:
+        switch (X / 10) {
+            case 0:
+                earlyList.dequeue(next);
+                RandomWaiting(next);
+                break;
+            case 1:
+                lateList.dequeue(next);
+                RandomWaiting(next);
+                break;
+            case 2:
                     
-                    break;
-                case 3:
+                break;
+            case 3:
                     
-                    break;
-                case 4:
+                break;
+            case 4:
                     
-                    break;
-                case 5:
+                break;
+            case 5:
                     
-                    break;
-                case 6:
+                break;
+            case 6:
                     
-                    break;
-                case 7:
+                break;
+            case 7:
                     
-                    break;
-                case 8:
+                break;
+            case 8:
                     
-                    break;
-                case 9:
+                break;
+            case 9:
                     
-                    break;
-                case 10:
+                break;
+            case 10:
                     
-                    break;
-                default:
-                    break;
-            }
+                break;
+            default:
+                break;
+        }
         }
 
-        void RandomWaiting(Patient* p, bool fromTreatment = false){
-            int randomNumber = (rand() % 101 );
+        void RandomWaiting(Patient* p, bool fromTreatment = false) {
+            int randomNumber = (rand() % 101);
 
-            if(fromTreatment || p->getPT() <= p->getVT()){) {
+            if (fromTreatment || p->getPT() <= p->getVT()) {
+                if (randomNumber < 33) {
+                    //E_WaitingList.enqueue(p);
+                }
 
+                if (randomNumber >= 33 && randomNumber < 66) {
+                    //U_WaitingList.enqueue(p);
+                }
 
-            if(randomNumber < 33 ){
-                        E_WaitingList.enqueue(p);
-        if(randomNumber >= 33 && randomNumber < 66 ) {
-                U_WaitingList.enqueue(p);
-
-
+                if (randomNumber > 66) {
+                    //X_WaitingList.enqueue(p)
+                }
             }
+            else {
+                int penalty = (p->getVT() - p->getPT()) / 2;
 
-            if(randomNumber > 66) {
-                X_WaitingList.enqueue(p)
-            }else{
-                int penalty = (topPatient->getVT() - topPatient->getPT()) / 2;
-
-                if(randomNumber < 33) {
-                        E_WaitingList.InsertSorted(p, p->getPT() + penalty);
+                if (randomNumber < 33) {
+                    //E_WaitingList.InsertSorted(p, p->getPT() + penalty);
                 }
 
-                if(randomNumber >= 33 && randomNumber < 66 ) {
-
-                        U_WaitingList.InsertSorted(p, p->getPT() + penalty);
-
+                if (randomNumber >= 33 && randomNumber < 66) {
+                    //U_WaitingList.InsertSorted(p, p->getPT() + penalty);
                 }
 
-                if(randomNumber > 66) {
-                        X_WaitingList.InsertSorted(p, p->getPT() + penalty);
-
+                if (randomNumber > 66) {
+                    //X_WaitingList.InsertSorted(p, p->getPT() + penalty);
                 };
             }
-        
+        }
 
 };
 
