8a61c1bbd71dc7896a7e1d404597a68c54df3408
A-Elbana <manelbana094@gmail.com>
2025-04-10 16:13:16 +0200

    Forgot to fix this in the last commit.

--- PyhsiotherapyCentre/Lists.h
+++ PyhsiotherapyCentre/Lists.h
@@ -20,7 +20,7 @@ struct Lists {
 
     M1Queue U_WaitingList;
     M1Queue E_WaitingList;
-    M2Queue<Patient*> X_WaitingList;
+    M2Queue X_WaitingList;
 
     LinkedQueue<UEResource*> U_Deivces;
     LinkedQueue<UEResource*> E_Deivces;
