3a51b208eaad9af31af21911c0dfdb4c3b8c2940
Abdallah Tarek <14712023100934@stud.cu.edu.eg>
2025-04-11 14:30:52 +0200

    Cancel done

--- PyhsiotherapyCentre/M2Queue.h
+++ PyhsiotherapyCentre/M2Queue.h
@@ -1,13 +1,32 @@
 #pragma once
 #include "M1Queue.h"
+#include "Patient.h"
 
 class M2Queue : public M1Queue
 {
-	/*	No Actual Cancel is Required in Phase 1
-	void Cancel(int index)
-	{
 
+	public:
+
+	void Cancel(int index,Patient*& Delinked)
+	{
+		if (index == 0)
+		{
+			Delinked = frontPtr->getItem();
+			frontPtr->setNext(frontPtr->getNext());
+			return;
+		}
+		Node<Patient*>* current = frontPtr->getNext();
+		Node<Patient*>* becurrent = frontPtr;
+		int i = 1;
+		while (i != index)
+		{
+			current = current->getNext();
+			becurrent = becurrent->getNext();
+			i++;
+		}
+		becurrent->setNext(current->getNext());
+		Delinked = current->getItem();
 	}
-	*/
+	
 };
 
