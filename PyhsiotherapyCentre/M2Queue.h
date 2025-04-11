93e2935de31b507f65c0559c881cbe65bc34384b
Abdallah Tarek <14712023100934@stud.cu.edu.eg>
2025-04-11 15:42:41 +0200

    count--

--- PyhsiotherapyCentre/M2Queue.h
+++ PyhsiotherapyCentre/M2Queue.h
@@ -13,6 +13,7 @@ class M2Queue : public M1Queue
 		{
 			Delinked = frontPtr->getItem();
 			frontPtr=frontPtr->getNext();
+			count--;
 			return;
 		}
 		Node<Patient*>* current = frontPtr->getNext();
